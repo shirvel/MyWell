c9a28c4f5222142c184d190ef974e703f9238aa0
ortalGolzarP <ortalgolzar@gmail.com>
2024-07-14 21:57:12 +0300

    add-user-model

--- backend/src/models/user_model.ts
+++ backend/src/models/user_model.ts
@@ -4,6 +4,12 @@ export interface IUser {
     email: string;
     password: string;
     name: string;
+    birthday: string,
+    gender: string,
+    mainGoal: string,
+    specialDiets: string,
+    healthConditions: string,
+    comment: string,
     _id?: string;
     refreshTokens?: string[];
     expiredTokens?: string[];
@@ -22,6 +28,30 @@ const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         required: true
+    },
+    birthday: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    mainGoal: {
+        type: String,
+        required: true
+    },
+    specialDiets: {
+      type: String,
+      required: true
+    },
+    healthConditions: {
+      type: String,
+      required: true
+    },
+    comment: {
+      type: String,
+      required: true
     },
     refreshTokens: {
         type: [String],
---
+++ backend/src/routes/user_routes.ts
@@ -0,0 +1,147 @@
+import express from "express";
+const router = express.Router();
+import User from '../controllers/user_controller';
+// import authMiddleware from '../common/auth_middleware';
+
+/**
+* @swagger
+* tags:
+*   name: User
+*   description: The user API
+*/
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - email
+*         - password
+*         - name
+*       properties:
+*         email:
+*           type: string
+*           description: The user email
+*         password:
+*           type: string
+*           description: The user password
+*         name:
+*           type: string
+*           description: The user name
+*       example:
+*         email: 'ortal@gmail.com'
+*         password: '123456'
+*         name: 'Ortal'
+*/
+
+/**
+* @swagger
+* /user/:
+*   get:
+*     summary: get all users
+*     tags: [User]
+*     responses:
+*       200:
+*         description: all the users
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
+router.get("/", User.getAllUsers);
+// router.get("/", authMiddleware, User.getAllUsers);
+
+/**
+* @swagger
+* /user/{id}:
+*   get:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user 
+*     summary: get user with this id
+*     tags: [User]
+*     responses:
+*       200:
+*         description: the user with this id
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
+router.get("/:id", User.getUserById);
+
+// router.get("/:id", authMiddleware, User.getUserById);
+
+
+// router.get("/get_by_name/:name", authMiddleware, User.getUserByName);
+router.get("/get_by_name/:name", User.getUserByName);
+
+
+/**
+* @swagger
+* /user/{id}:
+*   patch:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: update a user
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       204:
+*         description: the updated user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
+router.patch("/:id", User.putUserById);
+
+// router.patch("/:id", authMiddleware, User.putUserById);
+
+
+/**
+* @swagger
+* /user/{id}:
+*   delete:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: delete a user by id
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: success message
+*/
+
+router.delete("/:id", User.deleteUserById);
+
+// router.delete("/:id", authMiddleware, User.deleteUserById);
+
+
+export default router;

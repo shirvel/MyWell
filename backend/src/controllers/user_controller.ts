324c1e048b02db259ea847e7a559ca0f4bd298c6
ortalGolzarP <ortalgolzar@gmail.com>
2024-07-21 12:08:03 +0300

    fix-user-test-and-change-put-user

--- backend/src/controllers/user_controller.ts
+++ backend/src/controllers/user_controller.ts
@@ -38,19 +38,55 @@ const getUserById = async (req: Request, res: Response) => {
     }
 };
 
-const putUserById = async (req: Request, res: Response) => {
-       try {
-           let user;
-        if (req.body.email) {
-             user = await User.findOneAndUpdate({_id: req.params.id}, {"email" :req.body.email, "name": req.body.name});
+// const putUserById = async (req: Request, res: Response) => {
+//        try {
+//            let user;
+//         if (req.body.email) {
+//              user = await User.findOneAndUpdate({_id: req.params.id}, {"email" :req.body.email, "name": req.body.name});
         
-        }
-        res.status(204).json({"message": user});
+//         }
+//         res.status(204).json({"message": user});
             
-    } catch (err) {
+//     } catch (err) {
+//         res.status(500).json( {message: err.message} );
+//     }
+// };
+
+    const putUserById = async (req: Request, res: Response) => {
+    //     try {
+    //         let user;
+    //     if (req.body.email) {
+    //         user = await User.findOneAndUpdate({_id: req.params.id}, {"email" :req.body.email, "name": req.body.name});
+        
+    //     }
+    //     res.status(204).json({"message": user});
+            
+    // } 
+    try {
+        const updateFields: any = {};
+
+        if (req.body.email) updateFields.email = req.body.email;
+        if (req.body.name) updateFields.name = req.body.name;
+        if (req.body.birthday) updateFields.birthday = req.body.birthday;
+        if (req.body.gender) updateFields.gender = req.body.gender;
+        if (req.body.mainGoal) updateFields.mainGoal = req.body.mainGoal;
+        if (req.body.specialDiets) updateFields.specialDiets = req.body.specialDiets;
+        if (req.body.healthConditions) updateFields.healthConditions = req.body.healthConditions;
+        if (req.body.comment) updateFields.comment = req.body.comment;
+
+        const user = await User.findOneAndUpdate({ _id: req.params.id }, updateFields, { new: true });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(204).json({ "message": user });
+    } 
+    
+    catch (err) {
         res.status(500).json( {message: err.message} );
     }
-   };
+    };
 
    const deleteUserById = async (req: Request, res: Response) => {
     console.log("deleteUserById");

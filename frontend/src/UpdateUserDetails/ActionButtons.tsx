b60fd98918b2828bac9ac44b05f964faf869cc4e
ortalGolzarP <ortalgolzar@gmail.com>
2024-08-09 18:40:17 +0300

    continue-update-user-front-and-logic

--- frontend/src/UpdateUserDetails/ActionButtons.tsx
+++ frontend/src/UpdateUserDetails/ActionButtons.tsx
@@ -1,21 +1,27 @@
-import React, { FC } from 'react';
-import { Grid, Button } from '@mui/material';
-import { UserDetails } from '../UserDetails/UserDetails';
-
+import { FC, useState, useEffect } from 'react';
+import { Grid, Button, Snackbar, Alert } from '@mui/material';
 interface actions { 
     onSave: any;
 }
 
 const ActionButtons: FC<actions> = ({onSave}) => {
+    
+    const [showPopup, setShowPopup] = useState<boolean>(false)
+
+    useEffect(() => {
+        const popup = localStorage.getItem('popup');
+    
+        if (popup === 'true') {
+          setShowPopup(true);
+          localStorage.removeItem('popup');
+        }
+      }, []);
+
     const handleSubmit = () => {
+        localStorage.setItem('popup', 'true');
         onSave();
-        alert('Form submitted');
     };
 
-    const handleCancel = () => {
-        // Logic for canceling changes
-        alert('Changes canceled');
-    };
 
     const handleGenerateNewPlan = () => {
         // Logic for generating a new plan
@@ -23,23 +29,30 @@ const ActionButtons: FC<actions> = ({onSave}) => {
     };
 
     return (
-        <Grid container spacing={2} justifyContent="space-between" sx={{ mt: 2 }}>
-            <Grid item>
-                <Button variant="contained" color="primary" onClick={handleSubmit}>
-                    Save
-                </Button>
+        <>
+            <Grid container spacing={2} justifyContent="space-between" sx={{ mt: 2 }}>
+                <Grid item>
+                    <Button variant="contained" color="primary" onClick={handleSubmit}>
+                        Save
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button variant="outlined" onClick={handleGenerateNewPlan}>
+                        Generate New Plan
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Button variant="outlined" onClick={handleCancel}>
-                    Cancel
-                </Button>
-            </Grid>
-            <Grid item>
-                <Button variant="outlined" onClick={handleGenerateNewPlan}>
-                    Generate New Plan
-                </Button>
-            </Grid>
-        </Grid>
+            <Snackbar style={{width: '18vw', left: '41vw'}} open={showPopup} autoHideDuration={5000} 
+                onClose={() => {setShowPopup(false)}}>
+                <Alert
+                    severity="success"
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    Your details have been updated!
+                </Alert>
+                </Snackbar>
+        </>
     );
 };
 
--- frontend/src/UpdateUserDetails/UpdateUserDetailsService.tsx
+++ frontend/src/UpdateUserDetails/UpdateUserDetailsService.tsx
@@ -1,10 +1,14 @@
-import { UserDetails } from "../UserDetails/UserDetails";
+import { uploadImage } from "../Registration/image_requests";
 import { endpoints } from "../api/endpoints";
 import { patch } from "../api/requests";
 
-export const updateUserDetails = async (userDetails: any) => {
+export const updateUserDetails = async (userDetails: any, imageFile: File | undefined) => {
 	try {
         const connectedUserId = localStorage.getItem("userId");
+		if(imageFile !== undefined) {
+			const image_url = await uploadImage(imageFile);
+			userDetails.imageUrl = image_url;
+		}
         userDetails.mainGoal = userDetails.mainGoal.join(' & ')
         userDetails.specialDiets = userDetails.specialDiets.join(' & ')
 		const response = await patch(endpoints.USER.UPDATE_USER(connectedUserId!), userDetails);

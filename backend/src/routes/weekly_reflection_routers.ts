6e63e9672c3101a48b50bd6923d794b01f090bcb
ortalGolzarP <ortalgolzar@gmail.com>
2024-08-14 14:20:47 +0300

    add-middleware-to-all-routers

--- backend/src/routes/weekly_reflection_routers.ts
+++ backend/src/routes/weekly_reflection_routers.ts
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 import weekly_reflection_controller from '../controllers/weekly_reflection_controller';
+import authMiddleware from '../common/auth_middleware';
 
-router.post("/", weekly_reflection_controller.createfeedback);
-router.get("/", weekly_reflection_controller.getAllfeedback); 
+router.post("/", authMiddleware, weekly_reflection_controller.createfeedback);
+router.get("/", authMiddleware, weekly_reflection_controller.getAllfeedback); 
 export default router;
